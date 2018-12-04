82f168661f83af1e7e66a7dd204bb9893525fce9
User <User@YD03-17>
2018-12-04 16:57:17 +0900

    123123

--- YRTrip/src/main/java/com/yrtrip/app/travel/getTravelBoardList.java
+++ YRTrip/src/main/java/com/yrtrip/app/travel/getTravelBoardList.java
@@ -2,4 +2,13 @@ package com.yrtrip.app.travel;
 
 public class getTravelBoardList {
 
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
