ac908106d59632ab8a8cca2d6d064018375db898
pickyham <ringkle777@gmail.com>
2018-12-05 16:14:30 +0900

    혜림 product.css productVO

--- YRTrip/src/main/java/com/yrtrip/app/product/ProductVO.java
+++ YRTrip/src/main/java/com/yrtrip/app/product/ProductVO.java
@@ -1,111 +1,134 @@
 package com.yrtrip.app.product;
 
 public class ProductVO {
-	private int item_id;
-	private String item_name;
-	private int item_price;
-	private int item_ea;
-	private String item_content;
-	private String item_method;
-	private String item_category;
-	private String seller_id;
-	private String item_orderdetail;
-	private int item_star;
-	private String item_pic;
-	private String item_date;
-	private String item_condition;
-	private int item_like;
-	public int getItem_id() {
-		return item_id;
+	private int itemId;
+	private String itemName;
+	private int itemPrice;
+	private int itemEa;
+	private String itemContent;
+	private String itemMethod;
+	private String itemCategory;
+	private String sellerId;
+	private String itemOrderdetail;
+	private int itemStar;
+	private String itemPic;
+	private String itemDate;
+	private String itemCondition;
+	private int itemLike;
+	
+	public int getItemId() {
+		return itemId;
 	}
-	public void setItem_id(int item_id) {
-		this.item_id = item_id;
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
 	}
-	public String getItem_name() {
-		return item_name;
+	public String getItemName() {
+		return itemName;
 	}
-	public void setItem_name(String item_name) {
-		this.item_name = item_name;
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
 	}
-	public int getItem_price() {
-		return item_price;
+	public int getItemPrice() {
+		return itemPrice;
 	}
-	public void setItem_price(int item_price) {
-		this.item_price = item_price;
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
 	}
-	public int getItem_ea() {
-		return item_ea;
+	public int getItemEa() {
+		return itemEa;
 	}
-	public void setItem_ea(int item_ea) {
-		this.item_ea = item_ea;
+	public void setItemEa(int itemEa) {
+		this.itemEa = itemEa;
 	}
-	public String getItem_content() {
-		return item_content;
+	public String getItemContent() {
+		return itemContent;
 	}
-	public void setItem_content(String item_content) {
-		this.item_content = item_content;
+	public void setItemContent(String itemContent) {
+		this.itemContent = itemContent;
 	}
-	public String getItem_method() {
-		return item_method;
+	public String getItemMethod() {
+		return itemMethod;
 	}
-	public void setItem_method(String item_method) {
-		this.item_method = item_method;
+	public void setItemMethod(String itemMethod) {
+		this.itemMethod = itemMethod;
 	}
-	public String getItem_category() {
-		return item_category;
+	public String getItemCategory() {
+		return itemCategory;
 	}
-	public void setItem_category(String item_category) {
-		this.item_category = item_category;
+	public void setItemCategory(String itemCategory) {
+		this.itemCategory = itemCategory;
 	}
-	public String getSeller_id() {
-		return seller_id;
+	public String getSellerId() {
+		return sellerId;
 	}
-	public void setSeller_id(String seller_id) {
-		this.seller_id = seller_id;
+	public void setSellerId(String sellerId) {
+		this.sellerId = sellerId;
 	}
-	public String getItem_orderdetail() {
-		return item_orderdetail;
+	public String getItemOrderdetail() {
+		return itemOrderdetail;
 	}
-	public void setItem_orderdetail(String item_orderdetail) {
-		this.item_orderdetail = item_orderdetail;
+	public void setItemOrderdetail(String itemOrderdetail) {
+		this.itemOrderdetail = itemOrderdetail;
 	}
-	public int getItem_star() {
-		return item_star;
+	public int getItemStar() {
+		return itemStar;
 	}
-	public void setItem_star(int item_star) {
-		this.item_star = item_star;
+	public void setItemStar(int itemStar) {
+		this.itemStar = itemStar;
 	}
-	public String getItem_pic() {
-		return item_pic;
+	public String getItemPic() {
+		return itemPic;
 	}
-	public void setItem_pic(String item_pic) {
-		this.item_pic = item_pic;
+	public void setItemPic(String itemPic) {
+		this.itemPic = itemPic;
 	}
-	public String getItem_date() {
-		return item_date;
+	public String getItemDate() {
+		return itemDate;
 	}
-	public void setItem_date(String item_date) {
-		this.item_date = item_date; 
+	public void setItemDate(String itemDate) {
+		this.itemDate = itemDate;
 	}
-	public String getItem_condition() {
-		return item_condition;
+	public String getItemCondition() {
+		return itemCondition;
 	}
-	public void setItem_condition(String item_condition) {
-		this.item_condition = item_condition;
+	public void setItemCondition(String itemCondition) {
+		this.itemCondition = itemCondition;
 	}
-	public int getItem_like() {
-		return item_like;
+	public int getItemLike() {
+		return itemLike;
 	}
-	public void setItem_like(int item_like) {
-		this.item_like = item_like;
+	public void setItemLike(int itemLike) {
+		this.itemLike = itemLike;
 	}
 	
 	@Override
 	public String toString() {
-		return "BoardVO [item_id=" + item_id + ", item_name=" + item_name + ", item_price=" + item_price + ", item_ea="
-				+ item_ea + ", item_content=" + item_content + ", item_method=" + item_method + ", item_category="
-				+ item_category + ", seller_id=" + seller_id + ", item_orderdetail=" + item_orderdetail + ", item_star="
-				+ item_star + ", item_pic=" + item_pic + ", item_date=" + item_date + ", item_condition="
-				+ item_condition + ", item_like=" + item_like + "]";
-	}	
+		return "ProductVO [itemId=" + itemId + ", itemName=" + itemName + ", itemPrice=" + itemPrice + ", itemEa="
+				+ itemEa + ", itemContent=" + itemContent + ", itemMethod=" + itemMethod + ", itemCategory="
+				+ itemCategory + ", sellerId=" + sellerId + ", itemOrderdetail=" + itemOrderdetail + ", itemStar="
+				+ itemStar + ", itemPic=" + itemPic + ", itemDate=" + itemDate + ", itemCondition=" + itemCondition
+				+ ", itemLike=" + itemLike + "]";
+	}
+	
+	public ProductVO(int itemId, String itemName, int itemPrice, int itemEa, String itemContent, String itemMethod,
+			String itemCategory, String sellerId, String itemOrderdetail, int itemStar, String itemPic, String itemDate,
+			String itemCondition, int itemLike) {
+		super();
+		this.itemId = itemId;
+		this.itemName = itemName;
+		this.itemPrice = itemPrice;
+		this.itemEa = itemEa;
+		this.itemContent = itemContent;
+		this.itemMethod = itemMethod;
+		this.itemCategory = itemCategory;
+		this.sellerId = sellerId;
+		this.itemOrderdetail = itemOrderdetail;
+		this.itemStar = itemStar;
+		this.itemPic = itemPic;
+		this.itemDate = itemDate;
+		this.itemCondition = itemCondition;
+		this.itemLike = itemLike;
+	}
+	
+	
 }
