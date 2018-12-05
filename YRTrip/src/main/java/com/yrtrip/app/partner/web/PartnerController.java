086c8919fc3420a2d59918ef62f1eddf1eaa5856
User <User@YD03-17>
2018-12-05 16:29:50 +0900

    123123

--- YRTrip/src/main/java/com/yrtrip/app/partner/web/PartnerController.java
+++ YRTrip/src/main/java/com/yrtrip/app/partner/web/PartnerController.java
@@ -19,56 +19,55 @@ public class PartnerController {
 	PartnerBoardService partnerBoardService;
 
 	// 전체조회
-	@RequestMapping(value = { "/getPartnerBoardList", "/getPartnerListBoard",
-			"/getPartnerBoards" }, method = RequestMethod.GET) // http://localhost:8081/app/getBoardList
+	@RequestMapping(value = { "/getPartnerList"}, method = RequestMethod.GET) // http://localhost:8081/app/getPartnerList
 	public String getPartnerBoardList(Model model, PartnerVO vo) {
 		model.addAttribute("partnerboardList", partnerBoardService.getPartnerBoardList(vo));
 		return "board/getPartnerBoardList";
 	}
 
 	// 단건조회
-	@RequestMapping("/getPartnerBoard") // http://localhost:8081/app/getBoard
+	@RequestMapping("/getPartner") // http://localhost:8081/app/getPartner
 	public String getBoard(Model model, PartnerVO vo) {
 		model.addAttribute("board", partnerBoardService.getPartnerBoard(vo)); // vo : 조회할 게시글 번호 넘어가는 것
 		return "board/getPartnerBoard";
 	}
 
 	// 등록폼
-	@RequestMapping(value = { "/insertBoardform" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/insertPartnerform" }, method = RequestMethod.GET)
 	public String insertPartnerBoardform() {
 		return "board/insertBoard";
 	}
 
 	// 등록처리
-	@RequestMapping(value = { "insertPartnerBoard" }, method = RequestMethod.POST)
+	@RequestMapping(value = { "insertPartner" }, method = RequestMethod.POST)
 	public String insertPartnerBoard(PartnerVO vo) { // 커맨드 객체
 		partnerBoardService.insertPartnerBoard(vo); // 등록처
 		return "redirect:getBoardList"; // 목록요청
 	}
 
 	// 수정폼
-	@RequestMapping("/updatePartnerBoardform")
+	@RequestMapping("/updatePartnerform")
 	public String updateBoardform(Model model, PartnerVO vo) {
 		model.addAttribute("board", partnerBoardService.getPartnerBoard(vo));
 		return "board/updatePartnerBoard";
 	}
 
 	// 수정처리
-	@RequestMapping("updatePartnerBoard")
+	@RequestMapping("updatePartner")
 	public String updateBoard(PartnerVO vo) {
 		partnerBoardService.updatePartnerBoard(vo); // 수정처리
 		return "redirect:getBoardList"; // 목록요청
 	}
 
 	// 삭제처리
-	@RequestMapping("deletePartnerBoard")
+	@RequestMapping("deletePartner")
 	public String deleteBoard(PartnerVO vo) {
 		partnerBoardService.deletePartnerBoard(vo); // 삭제처리
 		return "redirect:getBoardList"; // 목록요청
 	}
 
 	// 선택삭제처리
-	@RequestMapping("deletePartnerBoardList")
+	@RequestMapping("deletePartnerList")
 	public String deleteBoardList(PartnerVO vo) {
 		partnerBoardService.deletePartnerBoardList(vo); // 삭제처리
 		return "redirect:getBoardList"; // 목록요청
