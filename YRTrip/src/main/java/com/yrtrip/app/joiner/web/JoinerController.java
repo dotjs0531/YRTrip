f080c6aee01adbd130d0bceb30df8130371ccd07
User <User@YD03-17>
2018-12-11 16:21:47 +0900

    조인클래스 - 컨트롤러 수정

--- YRTrip/src/main/java/com/yrtrip/app/joiner/web/JoinerController.java
+++ YRTrip/src/main/java/com/yrtrip/app/joiner/web/JoinerController.java
@@ -1,10 +1,12 @@
 package com.yrtrip.app.joiner.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yrtrip.app.joiner.JoinerService;
 import com.yrtrip.app.joiner.JoinerVO;
@@ -14,14 +16,7 @@ public class JoinerController {
 
 	@Autowired
 	JoinerService joinerService;
-
-	// 전체조회
-		@RequestMapping(value = { "/getJoinerList"}, method = RequestMethod.GET) // http://localhost:8081/app/getPartnerList
-		public String getJoinerList(Model model, JoinerVO vo) {
-			model.addAttribute("joinerList", joinerService.getJoinerList(vo));
-			return "redirect:getPartnerList";
-		}
-
+	
 	// 신청 처리
 	@RequestMapping(value = { "/insertJoiner" }, method = RequestMethod.POST)
 	public String insertJoiner(JoinerVO vo) { // 커맨드 객체
@@ -36,5 +31,11 @@ public class JoinerController {
 		return "redirect:getPartnerList"; // 목록요청
 	}
 	
+	@RequestMapping("getJoinerList")
+	@ResponseBody
+	public List<JoinerVO> getJoinerList(JoinerVO vo){
+		vo.setPageUnit(10);
+		return joinerService.getJoinerList(vo);
+	}
 	
 }
